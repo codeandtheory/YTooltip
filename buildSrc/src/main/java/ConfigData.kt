30b1c7abe0fdf97013e019d54e697a033f28fc64
Latittude <jay.challa@ymedialabs.com>
2022-08-17 18:03:11 +0530

    Dependencies management added in buildSrc

--- buildSrc/src/main/java/ConfigData.kt
+++
@@ -1,8 +0,0 @@
-object ConfigData {
-    const val compileSdkVersion = 30
-    const val buildToolsVersion = "30.0.3"
-    const val minSdkVersion = 21
-    const val targetSdkVersion = 30
-    const val versionCode = 1
-    const val versionName = "1.0.0"
-}
--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,8 +2,8 @@
  * To define plugins
  */
 object BuildPlugins {
-    val android by lazy { "com.android.tools.build:gradle:${Versions.gradlePlugin}" }
-    val kotlin by lazy { "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}" }
+    const val android =  "com.android.tools.build:gradle:${Versions.gradlePlugin}"
+    const val kotlin =  "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
 }
 
 /**
@@ -11,31 +11,31 @@ object BuildPlugins {
  */
 object Deps {
     // Kotlin
-    val core_ktx by lazy { "androidx.core:core-ktx:${Versions.core_ktx}" }
-    val kotlin by lazy { "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}" }
+    const val core_ktx = "androidx.core:core-ktx:${Versions.core_ktx}"
+    const val kotlin =  "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
 
     //Compose
-    val compose_ui by lazy { "androidx.compose.ui:ui:${Versions.compose_version}" }
-    val compose_material by lazy { "androidx.compose.material:material:${Versions.compose_version}" }
-    val compose_preview by lazy { "androidx.compose.ui:ui-tooling-preview:${Versions.compose_version}" }
+    const val compose_ui =  "androidx.compose.ui:ui:${Versions.compose_version}"
+    const val compose_material =  "androidx.compose.material:material:${Versions.compose_version}"
+    const val compose_preview =  "androidx.compose.ui:ui-tooling-preview:${Versions.compose_version}"
 
     //Android
-    val androidx_lifecycle by lazy { "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.androidx_lifecycle}" }
-    val androidx_activity_compose by lazy { "androidx.activity:activity-compose:${Versions.androidx_activity_compose}" }
+    const val androidx_lifecycle =  "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.androidx_lifecycle}"
+    const val androidx_activity_compose =  "androidx.activity:activity-compose:${Versions.androidx_activity_compose}"
 
     //Test
-    val junit by lazy { "junit:junit:${Versions.junit}" }
-    val ext_junit by lazy { "androidx.test.ext:junit:${Versions.ext_junit}" }
-    val espresso_core by lazy { "androidx.test.espresso:espresso-core:${Versions.espresso_core}" }
-    val ui_test_junit by lazy { "androidx.compose.ui:ui-test-junit4:${Versions.compose_version}" }
+    const val junit =  "junit:junit:${Versions.junit}"
+    const val ext_junit =  "androidx.test.ext:junit:${Versions.ext_junit}"
+    const val espresso_core =  "androidx.test.espresso:espresso-core:${Versions.espresso_core}"
+    const val ui_test_junit =  "androidx.compose.ui:ui-test-junit4:${Versions.compose_version}"
 
-    val ui_tooling by lazy { "androidx.compose.ui:ui-tooling:${Versions.compose_version}" }
-    val ui_test_manifest by lazy { "androidx.compose.ui:ui-test-manifest:${Versions.compose_version}" }
-    val kotlin_stdlib by lazy { "org.jetbrains.kotlin:kotlin-stdlib-jdk8" }
+    const val ui_tooling =  "androidx.compose.ui:ui-tooling:${Versions.compose_version}"
+    const val ui_test_manifest =  "androidx.compose.ui:ui-test-manifest:${Versions.compose_version}"
+    const val kotlin_stdlib =  "org.jetbrains.kotlin:kotlin-stdlib-jdk8"
 
     //component
-    val appcompat by lazy { "androidx.appcompat:appcompat:${Versions.appCompat}" }
-    val materialDesign by lazy { "com.google.android.material:material:${Versions.material}" }
+    const val appcompat =  "androidx.appcompat:appcompat:${Versions.appCompat}"
+    const val materialDesign =  "com.google.android.material:material:${Versions.material}"
 
-    val constraintLayout by lazy { "androidx.constraintlayout:constraintlayout-compose:${Versions.constraintLayout}" }
+    const val constraintLayout =  "androidx.constraintlayout:constraintlayout-compose:${Versions.constraintLayout}"
 }
